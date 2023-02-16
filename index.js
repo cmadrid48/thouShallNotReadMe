1849ab99f04dda17161e99bb836be4547c8be1a6
cmadrid48 <cmadrid48@gmail.com>
2023-02-16 14:28:27 -0800

    added some questions to index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+// Using "require" to fetch package
+
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+// Using Inquirer for User Prompts...
 
 inquirer.prompt(
     [
@@ -11,32 +15,32 @@ inquirer.prompt(
         },
         {
             type: 'imput',
-            message: 'What is the title of your Project?',
-            name: 'title',
+            message: 'Please provide a description of you Project:',
+            name: 'description',
             validate: (value)=>{ if(value){return true} else {return 'I need you to enter a value...'}},
         },
         {
             type: 'imput',
-            message: 'What is the title of your Project?',
-            name: 'title',
+            message: 'Please provide a detailed description of how to install your Project:',
+            name: 'install',
             validate: (value)=>{ if(value){return true} else {return 'I need you to enter a value...'}},
         },
         {
             type: 'imput',
-            message: 'What is the title of your Project?',
-            name: 'title',
+            message: 'Please provide directions, or an example on use:',
+            name: 'usage',
             validate: (value)=>{ if(value){return true} else {return 'I need you to enter a value...'}},
         },
         {
             type: 'imput',
-            message: 'What is the title of your Project?',
-            name: 'title',
+            message: 'List all contributions to this Project:',
+            name: 'credits',
             validate: (value)=>{ if(value){return true} else {return 'I need you to enter a value...'}},
         },
         {
             type: 'imput',
-            message: 'What is the title of your Project?',
-            name: 'title',
+            message: 'Please write any tests for your Project:',
+            name: 'test',
             validate: (value)=>{ if(value){return true} else {return 'I need you to enter a value...'}},
         },
         {
