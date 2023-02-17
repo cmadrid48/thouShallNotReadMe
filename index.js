f1e87a9e4dd5d6958ded259009e85ff675b28d5f
cmadrid48 <cmadrid48@gmail.com>
2023-02-16 17:28:10 -0800

    all badgelinks and sources linked for License

--- index.js
+++ index.js
@@ -76,35 +76,36 @@ const badgeLicense = (response) =>{
     let badge;
 
     if (response.license == 'GNU AGPLv3'){
-        badge = '[![License: AGPLv3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/lecence/agpl-3.0)';
+        badge = '[![License: AGPLv3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)';
     }
-    else if (response.license == 'GNU AGPLv3'){
-        badge = '[![License: AGPLv3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/lecence/agpl-3.0)';
+    else if (response.license == 'GNU GPLv3'){
+        badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://gnu.org/licenses/gpl-3.0)';
     }
-    else if (response.license == 'GNU AGPLv3'){
-        badge = '[![License: AGPLv3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/lecence/agpl-3.0)';
+    else if (response.license == 'GNU LGPLv3'){
+        badge = '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://gnu.org/licenses/lgpl-3.0)';
     }
-    else if (response.license == 'GNU AGPLv3'){
-        badge = '[![License: AGPLv3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/lecence/agpl-3.0)';
+    else if (response.license == 'Mozilla Public License 2.0'){
+        badge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
     }
-    else if (response.license == 'GNU AGPLv3'){
-        badge = '[![License: AGPLv3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/lecence/agpl-3.0)';
+    else if (response.license == 'Apache License 2.0'){
+        badge = '[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
     }
-    else if (response.license == 'GNU AGPLv3'){
-        badge = '[![License: AGPLv3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/lecence/agpl-3.0)';
+    else if (response.license == 'MIT License'){
+        badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource,org/licenses/MIT)';
     }
-    else if (response.license == 'GNU AGPLv3'){
-        badge = '[![License: AGPLv3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/lecence/agpl-3.0)';
+    else if (response.license == 'Boost Software License 1.0'){
+        badge = '[![License: Boost Software License 1.0](https://img.shields.io/badge/license-boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
     }
-    else if (response.license == 'GNU AGPLv3'){
-        badge = '[![License: AGPLv3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/lecence/agpl-3.0)';
+    else if (response.license == 'The Unlicense'){
+        badge = '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](https://unlicense.org/)';
     }
     return badge;
 };
 
 // Function creating readme file using template literal
 
-const createReadMe = (response) => `
+const createReadMe = (response) => 
+`
 # ${response.title}
 ${badgeLicense(response)}
 ## Description
